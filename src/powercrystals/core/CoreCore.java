29473e0fcc4822f9d6c573a128a397f7076974fc
powercrystals <powcrystals@gmail.com>
2013-02-18 00:27:21 -0500

    Added AT line for MFR

--- src/powercrystals/core/CoreCore.java
+++ src/powercrystals/core/CoreCore.java
@@ -21,7 +21,7 @@ import cpw.mods.fml.relauncher.Side;
 
 public class CoreCore extends DummyModContainer implements IUpdateableMod
 {
-	public static final String version = "1.4.6R1.0.2";
+	public static final String version = "1.4.6R1.0.3B1";
 	public static final String modId = "PowerCrystalsCore";
 	public static final String modName = "PowerCrystals Core";
 	

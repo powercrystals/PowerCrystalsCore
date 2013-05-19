c8e7710acd88f140c6eb1f32a96486145a3ae217
Emy <emy@fake.email>
2013-05-19 16:06:20 -0600

    IAirDropTarget is back, for compat with older MFR

--- src/powercrystals/core/util/UtilInventory.java
+++ src/powercrystals/core/util/UtilInventory.java
@@ -14,6 +14,7 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.world.World;
 import net.minecraftforge.common.ForgeDirection;
+import powercrystals.core.block.IAirDropTarget;
 import powercrystals.core.inventory.IInventoryManager;
 import powercrystals.core.inventory.InventoryManager;
 import powercrystals.core.position.BlockPosition;

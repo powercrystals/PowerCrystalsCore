4c84e6ada1280b85b47cc4a5fda6d5b59e401e4a
Emy <emy@fake.email>
2013-05-19 16:06:20 -0600

    IAirDropTarget is back, for compat with older MFR

--- src/powercrystals/core/block/IAirDropTarget.java
+++ src/powercrystals/core/block/IAirDropTarget.java
@@ -3,6 +3,7 @@ package powercrystals.core.block;
 /**
  * Defines a block that UtilInventory.dropStack is allowed to drop EntityItems into (in the world).
  */
+@Deprecated
 public interface IAirDropTarget
 {
 	

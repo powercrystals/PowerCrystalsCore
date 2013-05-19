953a81b96dc2c15b32ae07e42e4cb7302cb8268c
Emy <emy@fake.email>
2013-05-19 16:03:36 -0600

    Revert "Removed IAirDropTarget"
    This reverts commit 70094591a170fc58cf989b809cdf72e2e78425ad.

---
+++ src/powercrystals/core/block/IAirDropTarget.java
@@ -0,0 +1,9 @@
+package powercrystals.core.block;
+
+/**
+ * Defines a block that UtilInventory.dropStack is allowed to drop EntityItems into (in the world).
+ */
+public interface IAirDropTarget
+{
+	
+}
